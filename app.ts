aeae336a2fca547e8d7d025be082ef88d263a72e
arbezerra <arbezerra@gmail.com>
2023-06-22 16:51:46 -0300

    feat: CORS

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(cors());
 
 app.use("/health", async (_req: Request, res: Response) => {
   const healthcheck = {
