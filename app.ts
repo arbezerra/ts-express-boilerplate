1aa94c4c249f6d88e90eea37421212e30a0f80aa
arbezerra <arbezerra@gmail.com>
2023-06-22 17:05:02 -0300

    feat: JSON Parser

--- app.ts
+++ app.ts
@@ -9,6 +9,7 @@ const app: Express = express();
 
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
 
 app.use("/health", async (_req: Request, res: Response) => {
   const healthcheck = {
