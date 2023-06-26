1b11da9199530db24d3880195c96a4cda1b09c52
arbezerra <arbezerra@gmail.com>
2023-06-26 17:07:14 -0300

    test: Coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "babel",
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
